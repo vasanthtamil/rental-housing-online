3fce2fda1a5bd5f17f178ccd501be85b97c861b4
Thamilarasan <79365718+vasanthtamil@users.noreply.github.com>
2021-05-26 19:21:27 +0530

    Update UserProfileQuery.sql

--- Database/UserProfileQuery.sql
+++ Database/UserProfileQuery.sql
@@ -1,2 +1,3 @@
 ﻿create table UserProfile (name varchar(60) not null,email varchar(100) not null, password varchar(90) not null,mobile varchar(20) not null,category varchar(15) not null );
-insert into UserProfile (name,email,password,mobile,category) values ('Narayanan A','anarayanan345@gmail.com','Nar1234#','8883406468','houseowner');
+insert into UserProfile (name,email,password,mobile,category) values ('gokul','anarayanan345@gmail.com','Nar1234#','8883406468','houseowner');
+insert into UserProfile (name,email,password,mobile,category) values ('ThamilArasan','tamilarasan0024@gmail.com','Tha1234#','9488680902','Tenant');
